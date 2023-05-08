f02cbc45a36564ae3547a977a132eb7dd2201f9b
Gaangb <angelob4stos@gmail.com>
2023-05-08 16:55:12 -0300

    feat: adicionando imagem

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Image, Dimensions, Text } from "react-native"; // Dimentions serve para pegar o tamanho da screen e melhorar a responsividade
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta(){
-    return <Image source={}/>
+    return <>
+    <Image source={topo} style={estilos.topo}/>
+    <Text>Detalhes da Cesta</Text>
+   </>
 }
+
+const estilos = StyleSheet.create ({
+    topo:{
+        width: "100%",
+        height: 578 / 768 * width
+    }
+})
