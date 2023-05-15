743ef95a1e464c86ed637c1326f9212841cc7311
Gaangb <angelob4stos@gmail.com>
2023-05-15 17:10:24 -0300

    adicionando expo app loading

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import mock from './src/mocks/cesta'
+import AppLoading from 'expo-app-loading'; // expo install expo-app-loading
+
 export default function App() {
 
   const [fonteCarregada] = useFonts({
@@ -10,7 +12,7 @@ export default function App() {
   });
 
   if (!fonteCarregada){
-    return <View/>
+    return <AppLoading/>
   }
 
   return (
