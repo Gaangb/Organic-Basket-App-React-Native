4397c956d474ee50394a03a440955726d2825c3c
Gaangb <angelob4stos@gmail.com>
2023-05-16 17:13:53 -0300

    adicionando scrolview

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,20 +1,22 @@
 import React from "react"; // View é tipo uma div
-import { StyleSheet, View } from "react-native"; 
+import {ScrollView, StyleSheet, View } from "react-native"; //scroll view serve para permitir descer a tela
 import Topo from "./componentes/Topo/";
 import Detalhes from "./componentes/Detalhes";
+import Itens from "./componentes/Itens";
 
 
 
 
 
-export default function Cesta({topo, detalhes}){
-    return <>
-    <Topo {...topo}/>
+export default function Cesta({topo, detalhes, itens}){
+    return <ScrollView>
+        <Topo {...topo}/>
 
-    <View style={estilos.cesta}>
-        <Detalhes {...detalhes}/>
-    </View>
-   </>
+        <View style={estilos.cesta}>
+            <Detalhes {...detalhes}/>
+            <Itens {...itens}/>
+        </View>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create ({
